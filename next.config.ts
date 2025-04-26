743999f83b8479e8f11c6da5b8c5d4c35cd1141c
Arash Goodarzi <kiaksarg@gmail.com>
2025-04-26 21:32:53 +0200

    git commit -m "Turn off trailingSlash for GH Pages user-site"

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 // next.config.js
 import { withContentCollections } from "@content-collections/next";
 
-// replace this with your *actual* GitHub repo name:
-const repoName = "kiaksarg.github.io";
-
 export default withContentCollections({
   output: "export",
-  trailingSlash: true,
-  basePath: `/${repoName}`,       // ← applies to *all* pages
-  assetPrefix: `/${repoName}/`,   // ← makes client JS/JSON/CSS load from /<repoName>/_next/…
+  trailingSlash: false,      // ← switch to no-slash export
   pageExtensions: ["js","jsx","ts","tsx"],
   images: { unoptimized: true },
 });
